6e5ba8c2a32fac6da2878d01943d714e3129493a
Sumanth-rockzz <Sumanthn876@gmail.com>
2023-04-27 20:40:04 +0530

    Added useContext hook (authContext )

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,8 +1,9 @@
-import React, { useState, useEffect, useReducer } from "react";
+import React, { useState, useEffect, useReducer,useContext } from "react";
 
 import Card from "../UI/Card/Card";
 import classes from "./Login.module.css";
 import Button from "../UI/Button/Button";
+import AuthContext from "../../store/auth-context";
 
 const emailReducer = (state, action) => {
   if (action.type === "User_Input") {
@@ -38,6 +39,8 @@ const Login = (props) => {
     value: "",
     isValid: null,
   });
+
+ const authCtx= useContext(AuthContext);
 
   useEffect(() => {
     console.log("Fffect Running");
@@ -87,7 +90,7 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(emailState.value, passwordState.value);
+    authCtx.onlogin(emailState.value, passwordState.value);
   };
 
   return (
